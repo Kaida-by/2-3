aa4373a5f562338fe2a23cb2650cd2df67ee6a2c
Kaida-by <Skstolyarov@mail.ru>
2019-07-31 09:42:34 +0300

    Seven

--- autoload.php
+++ autoload.php
@@ -1,6 +1,7 @@
 <?php
 
 spl_autoload_register(function ($class) {
-    require __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
+    if (file_exists($class . '.php')) {
+        require __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
+    }
 });
-session_start();
--- index.php
+++ index.php
@@ -2,23 +2,20 @@
 
 require_once __DIR__ . '/autoload.php';
 
+session_start();
+
 $ctrl = $_GET['ctrl'] ?? 'ArticleController';
 
 $class = '\App\Controllers\\' . $ctrl;
 
 /**@var \App\Controller $ctrl*/
 
-if (isset($_GET['ctrl']) && isset($_GET['action'])) {
-    if (file_exists(__DIR__ . $class . '.php')) {
-        if (class_exists($class) && method_exists($class, $_GET['action'])) {
-            $ctrl = new $class;
-            $ctrl->action($_GET['action']);
-        } else {
-            echo 'Введите корректный экшн';
-        }
-    } else {
-        echo 'Такого контроллера не существует';
-    }
+if (isset($_GET['ctrl']) &&
+    isset($_GET['action']) &&
+    class_exists($class) &&
+    method_exists($class, $_GET['action'])) {
+    $ctrl = new $class;
+    $ctrl->action($_GET['action']);
 } else {
-    echo 'Введите ctrl или action';
+    echo 'Неправильный url';
 }
