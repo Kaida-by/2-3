a0833c0e053e99a3d30a28eefb7494c657c6a9cc
Kaida-by <Skstolyarov@mail.ru>
2019-08-27 20:23:31 +0300

    Eleven

--- App/Logger.php
+++ App/Logger.php
@@ -4,9 +4,19 @@ namespace App;
 
 class Logger
 {
-    public function setLog($message)
+    public function setLog($message, $file, $line)
     {
-        $log = date('Y-m-d H:i:s') . ' ' . print_r($message, true);
+        $log = date('Y-m-d H:i:s') .
+            "\r\n" .
+            print_r($message, true) .
+            "\r\n" .
+            'В файле: ' .
+            $file .
+            "\r\n" .
+            'На строке: ' .
+            $line .
+            "\r\n" .
+            '================================================================';
         file_put_contents(__DIR__ . '/../log/errors.txt', $log . PHP_EOL, FILE_APPEND);
     }
 }
--- index.php
+++ index.php
@@ -24,7 +24,7 @@ try {
     }
 } catch (DbException $error) {
     $log = new Logger();
-    $log->setLog('Ошибка в БД:  ' . $error->getMessage());
+    $log->setLog('Ошибка в БД:  ' . $error->getMessage(), $error->getFile(), $error->getLine());
     $view = new View();
     $view->display(__DIR__ . '/template/error.php');
 } catch (Errors $errors) {
@@ -33,7 +33,8 @@ try {
     $view->display(__DIR__ . '/template/errors.php');
 } catch (E404Exception $error) {
     $log = new Logger();
-    $log->setLog($error->getMessage());
+    $log->setLog($error->getMessage(), $error->getFile(), $error->getLine());
     $view = new View();
-    $view->display(__DIR__ . '/template/error.php');
+    $view->error = $error->getMessage();
+    $view->display(__DIR__ . '/template/error404.php');
 }
